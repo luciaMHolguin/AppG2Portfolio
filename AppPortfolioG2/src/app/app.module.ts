c6d84ad5d88938452973f38da2c27dc1c4a4bb3c
Lucia M <luciamholguin@gmail.com>
2019-02-12 12:50:07 +0100

    creando componente contenedor

--- AppPortfolioG2/src/app/app.module.ts
+++ AppPortfolioG2/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppContenedorComponent } from './app-contenedor/app-contenedor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppContenedorComponent
   ],
   imports: [
     BrowserModule,
