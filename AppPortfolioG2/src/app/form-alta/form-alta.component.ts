9db7ad795e5f55ab52a428fddbe0c032db850987
Joana <joanasanchez89@hotmail.com>
2019-02-12 14:46:42 +0100

    Código copiado de los componentes de alta y de la lista.

--- AppPortfolioG2/src/app/form-alta/form-alta.component.ts
+++ AppPortfolioG2/src/app/form-alta/form-alta.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-form-alta',
-  templateUrl: './form-alta.component.html',
-  styleUrls: ['./form-alta.component.css']
+  selector: "app-form-alta",
+  templateUrl: "./form-alta.component.html",
+  styleUrls: ["./form-alta.component.css"]
 })
 export class FormAltaComponent implements OnInit {
+  titulo: string;
+  etiqueta_nombre: string;
+  placeholder_nombre: string;
+  nombreIntroducido: string;
 
-  constructor() { }
+  descripcionIntroducida: string;
+  ficheroIntroducido: string;
+  servPortfolios: PortfolioService;
 
-  ngOnInit() {
+  constructor(sp: PortfolioService) {
+    this.servPortfolios = sp;
   }
 
+  ngOnInit() {
+    this.titulo = "Alta de portolios";
+    this.etiqueta_nombre = "Nombre";
+    this.placeholder_nombre = "Introduzca Nombre y apellidos";
+    this.nombreIntroducido = "El Innombrable";
+  }
+  limpiar() {
+    this.nombreIntroducido = "";
+    this.descripcionIntroducida = "";
+    this.ficheroIntroducido = "";
+    console.log("<<< Se supone que ha limpiado >>>");
+  }
+  darDeAlta() {
+    // Lo justo para llamar al servicio
+    this.servPortfolios.alta(
+      this.nombreIntroducido,
+      this.descripcionIntroducida,
+      this.ficheroIntroducido
+    );
+    console.log("<<< Se supone que ha dado de alta >>>");
+  }
 }
